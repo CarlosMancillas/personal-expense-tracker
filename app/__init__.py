02419d6d1380198b21879eda9bd064b151b9dd1c
unknown <carlosmancillas4799@gmail.com>
2026-05-27 11:50:05 -0700

    Initialize Flask App
    Initialize Flask app by modyfying the __init__.py file

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+
+
+db: SQLAlchemy = SQLAlchemy()
+
+login_manager: LoginManager = LoginManager()
+
+
+def create_app() -> Flask:
+    app: Flask = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+
+    from app.routes import main
+
+    app.register_blueprint(main)
+
+    return app
